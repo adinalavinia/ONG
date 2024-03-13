991d601ef7e6926c0d928e4e346d974380c952ee
Adina <adinaa_lavinia@yahoo.com>
2024-03-13 16:05:18 +0200

    hero section

--- ong/src/components/Hero/Hero.js
+++ ong/src/components/Hero/Hero.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Hero.css';
+import './HeroText.css';
 
 const Hero = () => {
   return (
-    <div className='hero'>
-      <video className='video-hero' src='/videos/video2.mp4' autoPlay loop muted></video>
+    <div className='hero container'>
+      <div className='video-text'>
+        <div className='video-hero'>
+          <video src='/videos/video2.mp4' autoPlay loop muted></video>
+        </div>
+        <div className='hero-text-container'>
+          <div className='hero-text'>
+            <h1>Vision for the future</h1>
+            <button className='btn'>Show more</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
